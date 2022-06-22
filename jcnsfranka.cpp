c84e1482340012825f380f85268552e2c8c00f13
jcns <jcns@nb-ictrl4.jcns.frm2.tum.de>
2022-06-22 09:55:38 +0200

    Cmakefile and JcnsFranka library templates

---
+++ jcnsfranka.cpp
@@ -0,0 +1,5 @@
+#include "jcnsfranka.h"
+
+JcnsFranka::JcnsFranka()
+{
+}
---
+++ jcnsfranka.h
@@ -0,0 +1,12 @@
+#ifndef JCNSFRANKA_H
+#define JCNSFRANKA_H
+
+#include "jcnsfranka_global.h"
+
+class JCNSFRANKA_EXPORT JcnsFranka
+{
+public:
+    JcnsFranka();
+};
+
+#endif // JCNSFRANKA_H
