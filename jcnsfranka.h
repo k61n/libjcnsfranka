67f3b13f324ce57f77ad109c8c6c3f44b856534f
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2022-09-12 16:25:24 +0200

    jcnsfranka: external C calls added

--- jcnsfranka.h
+++ jcnsfranka.h
@@ -32,4 +32,36 @@ private:
     double vmax = 1.7;  // [m s^-1]
 };
 
+
+extern "C"
+{
+    JcnsFranka* init(char *ip) {return new JcnsFranka(ip);}
+    void deinit(JcnsFranka* self) {delete self;}
+    double* readState(JcnsFranka* self)
+    {
+        Coordinates state = self->readState();
+        double *result = new double[10];
+        for (int i=0; i < 7; i++)
+            result[i] = state.joints[i];
+        for (int i=0; i < 3; i++)
+            result[i + 7] = state.xyz[i];
+        return result;
+    }
+    bool goHome(JcnsFranka* self) {return self->goHome();}
+    void moveJoints(JcnsFranka* self, double* joints)
+    {
+        std::array<double, 7> joints_arr;
+        for (int i = 0; i < 7; i++)
+            joints_arr[i] = joints[i];
+        return self->moveJoints(joints_arr);
+    }
+    void moveRelative(JcnsFranka* self, double dx, double dy, double dz) {return self->moveRelative(dx, dy, dz);}
+    void moveAbsolute(JcnsFranka* self, double x, double y, double z) {return self->moveAbsolute(x, y, z);}
+    bool isGripping(JcnsFranka* self) {return self->isGripping();}
+    void grasp(JcnsFranka* self) {return self->grasp();}
+    void release(JcnsFranka* self) {return self->release();}
+    void communicationTest(JcnsFranka* self) {return self->communicationTest();}
+}
+
+
 #endif // JCNSFRANKA_H
