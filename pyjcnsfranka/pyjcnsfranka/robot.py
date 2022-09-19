c5bf5a96c97b9260e84fa0487d40bc6b60f6a3b6
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2022-09-19 09:11:21 +0200

    pyjcnsfranka: removed unnecessary return statements

--- pyjcnsfranka/pyjcnsfranka/robot.py
+++ pyjcnsfranka/pyjcnsfranka/robot.py
@@ -33,32 +33,32 @@ class FrankaRobot:
         self.lib.moveJoints.argtypes = [c_void_p, POINTER(c_double)]
         self.lib.moveJoints.restype = None
         input = (c_double * len(joints))(*joints)
-        return self.lib.moveJoints(self.obj, input)
+        self.lib.moveJoints(self.obj, input)
 
     def move_relative(self, dx, dy, dz):
         self.lib.moveRelative.argtypes = [c_void_p, c_double, c_double, c_double]
         self.lib.moveRelative.restype = None
-        return self.lib.moveRelative(self.obj, dx, dy, dz)
+        self.lib.moveRelative(self.obj, dx, dy, dz)
 
     def move_absolute(self, x, y, z):
         self.lib.moveAbsolute.argtypes = [c_void_p, c_double, c_double, c_double]
         self.lib.moveAbsolute.restype = None
-        return self.lib.moveAbsolute(self.obj, x, y, z)
+        self.lib.moveAbsolute(self.obj, x, y, z)
 
     def is_gripping(self):
         self.lib.isGripping.argtypes = [c_void_p]
         self.lib.isGripping.restype = c_bool
-        return self.lib.isGripping(self.obj)
+        self.lib.isGripping(self.obj)
 
     def grasp(self):
         self.lib.grasp.argtypes = [c_void_p]
         self.lib.grasp.restype = None
-        return self.lib.grasp(self.obj)
+        self.lib.grasp(self.obj)
 
     def release(self):
         self.lib.release.argtypes = [c_void_p]
         self.lib.release.restype = None
-        return self.lib.release(self.obj)
+        self.lib.release(self.obj)
 
     def communication_test(self):
         self.lib.communicationTest.argtypes = [c_void_p]
