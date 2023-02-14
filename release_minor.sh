33e80052d98a51ce36162af4970dc62dd8838d21
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2023-02-14 13:38:47 +0100

    release routine as a bash script
    Change-Id: Ic00f519d80ba81227bd431f23ddd8cdc8996d67d
    Reviewed-on: https://forge.frm2.tum.de/review/c/jcns/tango/franka/+/30427
    Tested-by: Konstantin Kholostov <k.kholostov@fz-juelich.de>
    Reviewed-by: Konstantin Kholostov <k.kholostov@fz-juelich.de>

---
+++ release_minor.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+tag=$(git describe --tags --abbrev=0)
+major=$(echo $tag | cut -d. -f1)
+minor=$(echo $tag | cut -d. -f2)
+patch=$(echo $tag | cut -d. -f3)
+new_minor=$((minor + 1))
+new_tag="${major}.${new_minor}.${patch}"
+
+sed "s/VERSION ${tag#v}/VERSION ${new_tag#v}/" CMakeLists.txt > CMakeLists.txt.new
+mv CMakeLists.txt.new CMakeLists.txt
+
+header="jcnsfranka (${new_tag#v}) unstable; urgency=medium\n"
+changelog=$(git log --pretty=format:"  * %s" ${tag}..HEAD)
+sign="\n -- $(git config user.name) <$(git config user.email)> $(date -R)\n\n"
+echo -e "$header\n$changelog\n$sign$(cat debian/changelog)" > debian/changelog.new
+mv debian/changelog.new debian/changelog
+
+git add debian/changelog
+git add CMakeLists.txt
+git commit -m "Update changelog for $new_tag release"
+git tag -a $new_tag -m ""
+git push origin master --tags
