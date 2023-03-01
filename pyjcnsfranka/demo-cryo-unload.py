fde14e6fa573041e88c4bd78afe3c934bc424508
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2023-03-01 14:59:31 +0100

    load and unload sample demo for cryo setup
    Change-Id: I66d5761b96c5b991899f631045e6e41bf3bea805
    Reviewed-on: https://forge.frm2.tum.de/review/c/jcns/tango/franka/+/30549
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: Konstantin Kholostov <k.kholostov@fz-juelich.de>
    Reviewed-by: Konstantin Kholostov <k.kholostov@fz-juelich.de>

---
+++ pyjcnsfranka/demo-cryo-unload.py
@@ -0,0 +1,62 @@
+
+from pyjcnsfranka.robot import FrankaRobot
+
+
+flange_cryo = [0.443, -0.0025, 0.33]
+flange_stor = [0.362, -0.206, 0.454]
+sample_cryo = [0.446, -0.001, 0.182]
+sample_stor = [0.336, 0.12, 0.182]
+
+
+if __name__ == '__main__':
+    robot = FrankaRobot('192.168.1.2')
+    robot.go_home()
+
+    # go grab the flange
+    pos = robot.read_state()[7:]
+    robot.move_absolute(flange_cryo[0], flange_cryo[1], pos[2])
+    robot.move_absolute(flange_cryo[0], flange_cryo[1], flange_cryo[2])
+    robot.move_gripper(0.01)
+    robot.close_gripper(0, 70)
+
+    # lift up flange to the appropriate height
+    robot.move_absolute(flange_cryo[0], flange_cryo[1], flange_stor[2] + 0.02)
+
+    # now the flange has to travel to the store position
+    robot.move_absolute(flange_cryo[0] - 0.2, flange_cryo[1], flange_stor[2] + 0.02)
+    robot.move_absolute(flange_cryo[0] - 0.2, flange_stor[1], flange_stor[2] + 0.02)
+    robot.move_absolute(flange_stor[0], flange_stor[1], flange_stor[2] + 0.02)
+    robot.move_absolute(flange_stor[0], flange_stor[1], flange_stor[2])
+    robot.move_gripper(0.1)
+    robot.move_relative(0, 0, 0.02)
+
+    robot.go_home()
+    # grab sample from cryostat
+    pos = robot.read_state()[7:]
+    robot.move_absolute(sample_cryo[0], sample_cryo[1], pos[2])
+    robot.move_absolute(sample_cryo[0], sample_cryo[1], sample_cryo[2])
+    robot.move_gripper(0.01)
+    robot.close_gripper(0, 70)
+    robot.move_linear(0, 0, 0.02)
+    robot.move_absolute(sample_cryo[0], sample_cryo[1], pos[2])
+
+    # move to storage position
+    robot.move_absolute(sample_stor[0], sample_stor[1], pos[2])
+    robot.move_absolute(sample_stor[0], sample_stor[1], sample_stor[2] + 0.02)
+    robot.move_linear(0, 0, -0.02)
+    robot.move_gripper(0.1)
+    robot.move_absolute(sample_stor[0], sample_stor[1], pos[2])
+    robot.go_home()
+
+    # cover the flange
+    robot.move_absolute(flange_stor[0], flange_stor[1], pos[2])
+    robot.move_absolute(flange_stor[0], flange_stor[1], flange_stor[2])
+    robot.move_gripper(0.01)
+    robot.close_gripper(0, 70)
+    robot.move_absolute(flange_stor[0], flange_stor[1], flange_stor[2] + 0.02)
+    robot.move_absolute(flange_stor[0] - 0.1, flange_stor[1], flange_stor[2] + 0.02)
+    robot.move_absolute(flange_stor[0] - 0.1, flange_cryo[1], flange_stor[2] + 0.02)
+    robot.move_absolute(flange_cryo[0], flange_cryo[1], flange_stor[2] + 0.02)
+    robot.move_absolute(flange_cryo[0], flange_cryo[1], flange_cryo[2])
+    robot.move_gripper(0.1)
+    robot.go_home()
