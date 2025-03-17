9d32e9b7b7b9966d43690060279a9a467d78fac6
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2025-03-17 14:25:56 +0100

    pyjcnsfranka: move demo scripts to appropriate folder
    Change-Id: I0b5e22e301bd78a335dddd12b9ed9b4ba6584d48

--- python/demo.py
+++
@@ -1,9 +0,0 @@
-
-from pyjcnsfranka.routines import cryo_stick
-
-
-if __name__ == '__main__':
-    for i in range(100):
-        cryo_stick.load()
-        cryo_stick.unload()
-        print(f'Iteration {i} success')
