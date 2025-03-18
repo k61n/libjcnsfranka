c12a6e6802c09448b2974d657f004ae8b59e4d38
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2025-03-18 17:36:12 +0100

    jcnsfranka-demo: clang-tidy
    Change-Id: I7f152948b479edef55b9279df275cdd97fd8a748

--- demo/mainwindow.h
+++ demo/mainwindow.h
@@ -15,8 +15,8 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
 
 private slots:
     void on_connectBtn_clicked();
